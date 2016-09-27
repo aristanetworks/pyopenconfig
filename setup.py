86177915ae0d28471e42f325291708f3f2306fe7
Giuseppe Valente <gvalente@arista.com>
2016-09-27 15:04:12 -0700

    setup: match package version with tag

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyopenconfig",
-    version="0.1",
+    version="0.0.2",
     packages=find_packages(),
     scripts=['openconfig_client.py'],
 
