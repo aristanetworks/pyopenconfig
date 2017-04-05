ec92c0f29f45f2565f2eadf6efda30c07c9be0a0
Fabrice Rabaute <fabrice@arista.com>
2017-04-05 13:37:41 -0700

    Fix protoc command line and make script linux compatible

--- pyopenconfig/run_codegen.sh
+++ pyopenconfig/run_codegen.sh
@@ -5,10 +5,23 @@
 
 set -e
 
+case `uname -s` in
+  (Darwin)
+    SED='sed -i ""'
+    ;;
+  (Linux)
+    SED='sed -i'
+    ;;
+  (*)
+    echo >&2 'unsupported platform'
+    exit 1
+    ;;
+esac
+
 curl -s https://api.github.com/repos/openconfig/reference/git/refs/heads/master | jq -r .object.sha > reference_version
 version=$(cat reference_version)
 echo "Downloading openconfig.proto@$version"
 curl -s https://api.github.com/repos/openconfig/reference/contents/rpc/openconfig/openconfig.proto?ref=$version -H "Accept: application/vnd.github.VERSION.raw" > openconfig.proto
 # Fix up the import of any.proto (https://github.com/openconfig/reference/issues/24)
-sed -i "" "s/github.com\/golang\/protobuf\/ptypes\/any/google\/protobuf/" openconfig.proto
-protoc -I .:$GOPATH/src --python_out=. --grpc_out=. --plugin=protoc-gen-grpc=`which grpc_python_plugin` openconfig.proto
+$SED "s:github.com/golang/protobuf/ptypes/any:google/protobuf:" openconfig.proto
+python -m grpc_tools.protoc -I .:$GOPATH/src --python_out=. --grpc_python_out=. openconfig.proto
